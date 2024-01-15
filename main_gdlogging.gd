c961e99d481fd21ad6a6d0b0a7464fba613a1aad
raldone01 <raldone01@gmail.com>
2024-01-16 00:48:43 +0100

    Higher res icon and register it by code.

--- main_gdlogging.gd
+++ main_gdlogging.gd
@@ -6,3 +6,9 @@ func _enter_tree() -> void:
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("Log")
+
+func _get_plugin_icon():
+	return preload("assets/icons/plugin_icon_white.svg")
+
+func _get_plugin_name():
+	return "gdlogging"
